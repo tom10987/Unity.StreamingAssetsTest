5e552be96aba6387ec85016d124657e7af36b480
tom10987 <tom10987@gmail.com>
2016-11-11 01:06:32 +0900

    initial commit

---
+++ Assets/Scripts/StreamingAssetsTest.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StreamingAssetsTest : MonoBehaviour
+{
+  [SerializeField]
+  Text _text = null;
+
+  IEnumerator Start()
+  {
+    // ローカルフォルダにあるデータを読み込むには、file:// をパスの先頭につける
+    var path = "file://" + Application.streamingAssetsPath + "/test.txt";
+    Debug.Log(path);
+
+    // 指定したパスにあるデータを読み込む
+    // 本来はインターネット上にあるデータを取得するものだが、
+    // ローカルフォルダを指定することもできる
+    WWW www = new WWW(path);
+
+    if (!string.IsNullOrEmpty(www.error))
+    {
+      Debug.LogError(www.error);
+      yield break;
+    }
+
+    // 読み込み完了まで待機
+    yield return www;
+
+    _text.text = www.text;
+    Debug.Log("finish");
+  }
+}
